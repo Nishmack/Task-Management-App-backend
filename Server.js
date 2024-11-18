06866c3a75a7e62291d7b4aad7b2b4cc70ddc8d8
Nishma <nishmayuzuf@gmail.com>
2024-11-18 17:54:49 +0530

    first commit

--- Server.js
+++ Server.js
@@ -5,19 +5,14 @@ const connectDB = require('./config/db');
 const taskRoutes = require('./routes/taskRoutes');
 
 dotenv.config();
-
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to DB
 connectDB();
 
-// Routes
 app.use('/api/tasks', taskRoutes);
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
